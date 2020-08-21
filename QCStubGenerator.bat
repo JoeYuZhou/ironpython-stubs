9956d4754f3f8fa1aa915bd6a736699de1e556b5
JoeYuZhou <68843080+JoeYuZhou@users.noreply.github.com>
2020-08-21 08:58:26 +0100

    Update QCStubGenerator.bat
    safer delete

--- QCStubGenerator.bat
+++ QCStubGenerator.bat
@@ -1,12 +1,8 @@
 set project_path=C:\Projects\QuantConnect\JoeYuZhou\ironpython-stubs
 set ironpython_path=E:\Projects\QuantConnect\IronPythonNew\ironpython2\bin\Debug\net45
-REM cd %project_path%\release\stubs\QuantConnect
-REM DEL /f /q /s *.* > NUL
-REM cd ..
-REM rmdir /q /s QuantConnect
-REM cd ..
-REM cd ..
+cd %project_path%
+rmdir /q /s release\stubs\QuantConnect
 %ironpython_path%\ipy -X:FullFrames -m ironstubs make --all --keep-partial
-python -m stubsGenerator --path=%project_path%\\release\stubs\QuantConnect --partition
+python -m stubsGenerator --path=%project_path%\release\stubs\QuantConnect --partition
 
 
